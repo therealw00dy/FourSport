bbcd7103796ff8a792282cc55df9933d03283c38
Preston Woodrum <pwoody999@gmail.com>
2026-09-07 21:40:11 -0400

    Fixed my team model to work with the correct design

--- backend/src/models/Team.js
+++ backend/src/models/Team.js
@@ -2,8 +2,17 @@ const mongoose = require("mongoose");
 
 const TeamSchema = new mongoose.Schema({
     name: { type: String, required: true},
-    PF: { type: Number, default: 0},
-    PA: { type: Number, default: 0 }
+    owner: { type: String }, // Swap to user ref later after adding auth
 });
+
+TeamSchema.virtual("roster", {
+    ref: "Player",
+    localField: "_id",
+    foreignField: "fantasyTeam",
+}),
+
+TeamSchema.set("toJSON", { virtuals: true });
+TeamSchema.set("toObject", { virtuals: true });
+
 
 module.exports = mongoose.model("Team", TeamSchema);
